5d438be949ff64d670b902459b122a6004fab987
Wahid <wahidas@stud.ntnu.no>
2025-04-01 21:59:30 +0200

    Fix joint name consistency across Qube project
    - Update RViz configuration for better visualization
    - Fix motor_joint naming in controllers.yaml
    - Update PID controller to use motor_joint instead of rotary_joint
    - Add proper hardware interface configuration for simulation mode

--- qube_controller/setup.py
+++ qube_controller/setup.py
@@ -1,11 +1,11 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
 package_name = 'qube_controller'
 
 setup(
     name=package_name,
-    version='0.0.0',
-    packages=find_packages(exclude=['test']),
+    version='0.0.1',
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
@@ -15,11 +15,12 @@ setup(
     zip_safe=True,
     maintainer='wahid',
     maintainer_email='wahidas@stud.ntnu.no',
-    description='TODO: Package description',
+    description='Quanser Qube controller package',
     license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
+            'pid_controller = qube_controller.pid_controller:main',
         ],
     },
 )
