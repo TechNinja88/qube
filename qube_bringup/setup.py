5960b1fe4543c0a45973c724f2d394837b9dbee6
TechNinja88 <wahidas@stud.ntnu.no>
2025-04-14 01:14:05 +0200

    lagde readme til bring up filen.

--- qube_bringup/setup.py
+++ qube_bringup/setup.py
@@ -1,6 +1,8 @@
-from setuptools import setup
 import os
 from glob import glob
+
+from setuptools import setup
+
 package_name = 'qube_bringup'
 setup(
     name=package_name,
@@ -8,11 +10,11 @@ setup(
     packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
+         ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')), # Launch files
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')), # URDF files
-        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')), # RViz files
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),  # Launch files
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/*.xacro')),  # URDF files
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*.rviz')),  # RViz files
     ],
     install_requires=['setuptools'],
     zip_safe=True,
